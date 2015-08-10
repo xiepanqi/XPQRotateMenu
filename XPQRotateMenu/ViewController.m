c012a46fd2fbb938d291c358674e18198d267b88
xiepanqi <xiepanqi@xiepanqideMac-mini.local>
2015-08-10 21:37:06 +0800

    演示代码

--- XPQRotateMenu/ViewController.m
+++ XPQRotateMenu/ViewController.m
@@ -18,7 +18,16 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    NSArray *arr = @[@"菜单1",@"菜单2",@"菜单3",@"菜单4",@"菜单5",@"菜单6",@"菜单7",@"菜单8",@"菜单9",@"菜单10"];
+    NSMutableAttributedString *attriString = [[NSMutableAttributedString alloc] initWithString:@"我是一个特别的菜单"];
+    [attriString addAttribute:NSForegroundColorAttributeName value:[UIColor blueColor] range:NSMakeRange(0,4)];
+    [attriString addAttribute:NSForegroundColorAttributeName value:[UIColor redColor] range:NSMakeRange(4,3)];
+    [attriString addAttribute:NSForegroundColorAttributeName value:[UIColor greenColor] range:NSMakeRange(7,2)];
+    [attriString addAttribute:NSFontAttributeName value:[UIFont fontWithName:@"Arial-BoldItalicMT" size:20.0] range:NSMakeRange(0, 4)];
+    [attriString addAttribute:NSFontAttributeName value:[UIFont fontWithName:@"HelveticaNeue-Bold" size:20.0] range:NSMakeRange(4, 3)];
+    [attriString addAttribute:NSFontAttributeName value:[UIFont fontWithName:@"Courier-BoldOblique" size:20.0] range:NSMakeRange(7, 2)];
+
+    
+    NSArray *arr = @[@"菜单1",@"菜单2",@"菜单3",@"菜单4",attriString,@"菜单6",@"菜单7",@"菜单8",@"菜单9"];
     XPQRotateMenu *menu = [[XPQRotateMenu alloc] initWithTitleArray:arr];
     menu.intersectionImage = [UIImage imageNamed:@"cancleIcon"];
     [self.view addSubview:menu];
